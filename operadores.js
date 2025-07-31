9b8e621d23dca1ddc1202a6dec9976b8a2dfc975
carlos2006-prog <carlosjosuegarciaalvarado2006@gmail.com>
2025-07-30 21:06:22 -0600

    operadores.js

---
+++ operadores.js
@@ -0,0 +1,29 @@
+// Ejercicio 1
+let entradaValida = true;
+let edad = 15;
+
+if (entradaValida && edad > 13) {
+    console.log("Puede entrar al cine.");
+} else {
+    console.log("No puede entrar al cine.");
+}
+
+// Ejercicio 2
+edad = 65;
+let discapacitado = false;
+
+if (edad > 60 || discapacitado) {
+    console.log("Recibe descuento especial.");
+} else {
+    console.log("No recibe descuento especial.");
+}
+
+// Ejercicio 3
+let nota = 6;
+let pasoRecuperacion = true;
+
+if (nota >= 7 || pasoRecuperacion) {
+    console.log("Estudiante aprueba.");
+} else {
+    console.log("Estudiante no aprueba.");
+}
